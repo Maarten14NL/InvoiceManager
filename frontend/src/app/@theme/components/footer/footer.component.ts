022b7b4d8cbab748e68f7fd9cf26866023ef96a4
Maarten Jakobs <maarten.jakobs@gmail.com>
2020-06-16 15:15:17 +0200

    frontend personal touch

--- frontend/src/app/@theme/components/footer/footer.component.ts
+++ frontend/src/app/@theme/components/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
-  template: '',
+  templateUrl: './footer.component.html',
 })
 export class FooterComponent {
 }
